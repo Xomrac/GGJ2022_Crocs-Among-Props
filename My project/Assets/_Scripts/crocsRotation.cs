032e4d0f930444719ebcfe3953ab47ca754782a4
mikenigro <mikenigro99@gmail.com>
2022-01-29 19:52:44 +0100

    added inclinazione

--- My project/Assets/_Scripts/crocsRotation.cs
+++ My project/Assets/_Scripts/crocsRotation.cs
@@ -3,12 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crocsRotation : MonoBehaviour {
+public class crocsRotation : MonoBehaviour
+{
 	private void FixedUpdate() {
 		RaycastHit hit;
-		if (Physics.Raycast(transform.position, -transform.up, out hit, 5)) {
-			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal)), Time.fixedDeltaTime);
-		}
-	}
+		if (Physics.Raycast(transform.position, -transform.up, out hit, 5))
+		{
+			Debug.DrawRay(transform.position, -transform.up * 5, Color.black);
+			//transform.up = hit.normal;
+			transform.rotation=Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(Vector3.Cross(transform.right, hit.normal)), Time.fixedDeltaTime);
+			Debug.Log(hit.normal);
+			Debug.Log(hit.collider.gameObject.name);
+		}	}
 
+	
 }
